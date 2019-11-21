e9cad7d8654be73e02d89d641a12f7b505376377
eso <eso@esotericist.org>
2019-11-21 09:48:19 -0800

    restructuring to ensmallen main file

--- src/main/java/org/esotericist/mindshaft/Mindshaft.java
+++ src/main/java/org/esotericist/mindshaft/Mindshaft.java
@@ -90,89 +90,6 @@ public class Mindshaft
     private zoomstate zoom =  new zoomstate();
     
     
-    class zoomspec {
-        public int x;
-        public int z;
-        public int w;
-        public int h;
-        public double minU;
-        public double minV;
-        public double maxU;
-        public double maxV;
-        public float layerrate;
-        public int overdraw;
-        
-        public void setZoomSpec(int x, int z, int w, int h, 
-            double minU, double minV, double maxU, double maxV, float layerrate, int overdraw) {
-            
-            this.x = x;
-            this.z = z;
-            this.w = w;
-            this.h = h;
-            this.minU = minU;
-            this.minV = minV;
-            this.maxU = maxU;
-            this.maxV = maxV;
-            this.layerrate = layerrate;
-            this.overdraw = overdraw;
-        }
-
-        public void setZoomSpec( int size, float layerrate, int overdraw ) {
-            int w = size;
-            int h = size;
-            int x = -(w / 2) + 1;
-            int z = x;
-            double minU = (128 + x) * 1 / 256D;
-            double minV = minU;
-            double maxU = 1D - minU;
-            double maxV = maxU;
-            setZoomSpec(x, z, w, h, minU, minV, maxU, maxV, layerrate, overdraw);
-        }
-    }
-    
-    class zoomstate  {
-        public boolean fullscreen = false;
-
-        public int getZoom() {
-            int curzoom;
-            if( fullscreen ) {
-                curzoom = mindshaftConfig.zoomfs;
-            } else {
-                curzoom = mindshaftConfig.zoom;
-            }
-            if( curzoom >= zoomlist.length ) {
-                curzoom = zoomlist.length - 1;
-            }
-            return curzoom;
-        }
-
-        public void nextZoom() {
-            int newzoom = fullscreen ? mindshaftConfig.zoomfs : mindshaftConfig.zoom;
-            newzoom++;
-            if( newzoom >= zoomlist.length ) {
-                newzoom = 0;
-            }
-            if( fullscreen ) {
-                mindshaftConfig.setFSZoom(newzoom);
-            } else {
-                mindshaftConfig.setZoom(newzoom);
-            }
-        }
-
-        public void prevZoom() {
-            int newzoom = fullscreen ? mindshaftConfig.zoomfs : mindshaftConfig.zoom;
-            newzoom--;
-            if( newzoom < 0 ) {
-                newzoom = zoomlist.length - 1;
-            }
-            if( fullscreen ) {
-                mindshaftConfig.setFSZoom(newzoom);
-            } else {
-                mindshaftConfig.setZoom(newzoom);
-            }
-        }
-    }
-    
     
     // not currently used, keeping it around in case that changes again
     /*
@@ -563,6 +480,7 @@ public class Mindshaft
         for( int i = 0; i < zoomcount; ++i ) {
             logger.info("zoomlist: " + i +  ", x:" + zoomlist[i].x + ", z:" + zoomlist[i].z + ", w:" + zoomlist[i].w + ", minU:" + zoomlist[i].minU + ", minV:" + zoomlist[i].minV + ", maxU:" + zoomlist[i].maxU + ", maxV:" + zoomlist[i].maxV );
         }
+        zoom.zoommax = zoomlist.length;
     }
 
 
@@ -578,6 +496,7 @@ public class Mindshaft
             if( mindshaftConfig.zoomfs > zoomlist.length ) {
                 mindshaftConfig.setFSZoom(zoomlist.length - 1 );
             }
+            zoom.zoommax = zoomlist.length;
         }
     }
 
---
+++ src/main/java/org/esotericist/mindshaft/zoomhandling.java
@@ -0,0 +1,85 @@
+package org.esotericist.mindshaft;
+
+class zoomspec {
+    public int x;
+    public int z;
+    public int w;
+    public int h;
+    public double minU;
+    public double minV;
+    public double maxU;
+    public double maxV;
+    public float layerrate;
+    public int overdraw;
+    
+    public void setZoomSpec(int x, int z, int w, int h, 
+        double minU, double minV, double maxU, double maxV, float layerrate, int overdraw) {
+        
+        this.x = x;
+        this.z = z;
+        this.w = w;
+        this.h = h;
+        this.minU = minU;
+        this.minV = minV;
+        this.maxU = maxU;
+        this.maxV = maxV;
+        this.layerrate = layerrate;
+        this.overdraw = overdraw;
+    }
+
+    public void setZoomSpec( int size, float layerrate, int overdraw ) {
+        int w = size;
+        int h = size;
+        int x = -(w / 2) + 1;
+        int z = x;
+        double minU = (128 + x) * 1 / 256D;
+        double minV = minU;
+        double maxU = 1D - minU;
+        double maxV = maxU;
+        setZoomSpec(x, z, w, h, minU, minV, maxU, maxV, layerrate, overdraw);
+    }
+}
+
+class zoomstate  {
+    public boolean fullscreen = false;
+    public int zoommax = 0;
+
+    public int getZoom() {
+        int curzoom;
+        if( fullscreen ) {
+            curzoom = mindshaftConfig.zoomfs;
+        } else {
+            curzoom = mindshaftConfig.zoom;
+        }
+        if( curzoom >= zoommax ) {
+            curzoom = zoommax - 1;
+        }
+        return curzoom;
+    }
+
+    public void nextZoom() {
+        int newzoom = fullscreen ? mindshaftConfig.zoomfs : mindshaftConfig.zoom;
+        newzoom++;
+        if( newzoom >= zoommax ) {
+            newzoom = 0;
+        }
+        if( fullscreen ) {
+            mindshaftConfig.setFSZoom(newzoom);
+        } else {
+            mindshaftConfig.setZoom(newzoom);
+        }
+    }
+
+    public void prevZoom() {
+        int newzoom = fullscreen ? mindshaftConfig.zoomfs : mindshaftConfig.zoom;
+        newzoom--;
+        if( newzoom < 0 ) {
+            newzoom = zoommax - 1;
+        }
+        if( fullscreen ) {
+            mindshaftConfig.setFSZoom(newzoom);
+        } else {
+            mindshaftConfig.setZoom(newzoom);
+        }
+    }
+}
