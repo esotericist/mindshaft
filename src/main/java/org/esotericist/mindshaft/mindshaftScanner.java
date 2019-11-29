444debbf4429fbafcf1edc93059aab424451d360
eso <eso@esotericist.org>
2019-11-28 18:02:23 -0800

    excise chunk cache, work with only layerSegment caching

--- src/main/java/org/esotericist/mindshaft/mindshaftScanner.java
+++ src/main/java/org/esotericist/mindshaft/mindshaftScanner.java
@@ -16,7 +16,6 @@ import java.util.Objects;
 import java.util.Random;
 import java.util.Set;
 import java.util.Arrays;
-import java.util.BitSet;
 import java.util.Iterator;
 
 class mindshaftScanner {
@@ -26,6 +25,11 @@ class mindshaftScanner {
     private static long now = 0;
     private static int currentDim = 0;
     private static int currentTick = 0;
+
+    // cache values for player X and Z chunk coordinate
+    // this is necessary for temporal stability in texture offsets while rendering
+    private static int pX;
+    private static int pZ;
 
     // fudge for player's current Y level
     private static final double fudgeY = 17 / 32D;
@@ -40,40 +44,42 @@ class mindshaftScanner {
     // actual forced expiration time is forcedExpiry + expiry
     private static final int forcedExpiry = 800;
 
-    // random addition in ticks to forcedExpiry
-    private static final int forcedExpiryFudge = 1000;
-
     // default color for empty layers. dark green.
     private static final int defaultColor = 0x002200;
 
     // private layerSegment emptyLayer = new layerSegment(defaultColor);
 
-    static class chunkID {
-        int dimension, x, z;
+    static class segmentID {
+        int dimension, x, y, z;
 
         @Override
         public int hashCode() {
-            return Objects.hash(dimension, x, z);
+            return Objects.hash(dimension, x, y, z);
         }
 
         @Override
         public boolean equals(Object obj) {
             if (this == obj)
                 return true;
-            if (!(obj instanceof chunkID))
+            if (!(obj instanceof segmentID))
                 return false;
-            chunkID other = (chunkID) obj;
-            return dimension == other.dimension && x == other.x && z == other.z;
+            segmentID other = (segmentID) obj;
+            return dimension == other.dimension && x == other.x && y == other.y && z == other.z;
         }
 
-        public chunkID(int dim, int newX, int newZ) {
+        public segmentID(int dim, int newX, int newY, int newZ) {
             dimension = dim;
             x = newX;
+            y = newY;
             z = newZ;
         }
+
     }
 
     static class layerSegment {
+        long expiration = 0;
+        boolean stale = false;
+
         private int[] color = new int[256];
 
         public layerSegment(Integer v) {
@@ -89,67 +95,11 @@ class mindshaftScanner {
         }
     }
 
-    static class chunkData {
-        long expiration = 0;
 
-        BitSet solid = new BitSet(65536);
-        BitSet intangible = new BitSet(65536);
-        BitSet empty = new BitSet(65536);
-        BitSet lit = new BitSet(65536);
-
-        boolean stale = false;
-        boolean expired = false;
-        LinkedHashMap<Integer, layerSegment> layers = new LinkedHashMap<Integer, layerSegment>(16, 0.75f, true);
-
-        int calcOffset(int x, int y, int z) {
-            return z + 16 * (y + 256 * x);
-        }
-
-        public boolean getSolid(int x, int y, int z) {
-            return solid.get(calcOffset(x, y, z));
-        }
-
-        public void setSolid(int x, int y, int z, boolean b) {
-            solid.set(calcOffset(x, y, z), b);
-        }
-
-        public boolean getIntangible(int x, int y, int z) {
-            return intangible.get(calcOffset(x, y, z));
-        }
-
-        public void setIntangible(int x, int y, int z, boolean b) {
-            intangible.set(calcOffset(x, y, z), b);
-        }
-
-        public boolean getEmpty(int x, int y, int z) {
-            return empty.get(calcOffset(x, y, z));
-        }
-
-        public void setEmpty(int x, int y, int z, boolean b) {
-            empty.set(calcOffset(x, y, z), b);
-        }
-
-        public boolean getLit(int x, int y, int z) {
-            return lit.get(calcOffset(x, y, z));
-        }
-
-        public void setLit(int x, int y, int z, boolean b) {
-            lit.set(calcOffset(x, y, z), b);
-        }
-
-        public chunkData() {
-            solid.set(0, 65536, true);
-            /*
-             * for (int i = 0; i < 16; i++) { for (int j = 0; j < 256; j++) { for (int k =
-             * 0; k < 16; k++) { blockData[i][j][k] = new block(); } } }
-             */
-        }
-    }
-
-    static class chunkCache extends LinkedHashMap<chunkID, chunkData> {
-        protected boolean removeEldestEntry(Map.Entry<chunkID, chunkData> eldest) {
-            chunkData thisChunk = eldest.getValue();
-            if (thisChunk.expired || (thisChunk.stale && thisChunk.expiration <= mindshaftScanner.now)) {
+    static class segmentCache extends LinkedHashMap<segmentID, layerSegment> {
+        protected boolean removeEldestEntry(Map.Entry<segmentID, layerSegment> eldest) {
+            layerSegment thisSegment = eldest.getValue();
+            if ((thisSegment.stale && thisSegment.expiration <= mindshaftScanner.now)) {
                 // Mindshaft.logger.info(now + ": removed chunk: " + eldest.getKey().x + ", " +
                 // eldest.getKey().z);
                 return true;
@@ -158,13 +108,13 @@ class mindshaftScanner {
             }
         }
 
-        public chunkCache(Integer i) {
+        public segmentCache(Integer i) {
             super();
         }
     }
 
-    static chunkCache chunksKnown = new chunkCache(32);
-    static LinkedList<chunkID> requestedChunks = new LinkedList<chunkID>();
+    static segmentCache segmentsKnown = new segmentCache(512);
+    static LinkedList<segmentID> requestedSegments = new LinkedList<segmentID>();
 
     private int clamp(int value, int min, int max) {
         return Math.min(Math.max(value, min), max);
@@ -179,33 +129,7 @@ class mindshaftScanner {
         return false;
     }
 
-    private void requestChunk(chunkID chunk) {
-        if (!requestedChunks.contains(chunk)) {
-            // Mindshaft.logger.info(now+ ": requested chunk: " + chunk.x + ", " + chunk.z);
-            requestedChunks.add(chunk);
-        }
-    }
-
-    chunkData getChunk(chunkID chunk) {
-        chunkData thischunk = chunksKnown.get(chunk);
-        if (thischunk != null) {
-            if (!thischunk.stale && thischunk.expiration <= now) {
-                // Mindshaft.logger.info(now + ": stale chunk: " + chunk.x + ", " + chunk.z + ",
-                // stale at:" + thischunk.expiration );
-                thischunk.stale = true;
-                thischunk.expiration = now + forcedExpiry + random.nextInt(forcedExpiryFudge);
-                requestChunk(chunk);
-            }
-            // Mindshaft.logger.info(now + ": fetched chunk: " + chunk.x + ", " + chunk.z +
-            // ", stale at:" + thischunk.expiration );
-            return chunksKnown.get(chunk);
-        } else {
-            requestChunk(chunk);
-        }
-        return null;
-    }
-
-    int processColumn(World world, chunkID chunk, int x, int z, int pY) {
+    int processColumn(World world, segmentID ID, int x, int z) {
         int color = defaultColor;
         int red = 0;
         int blue = 0;
@@ -213,26 +137,46 @@ class mindshaftScanner {
 
         int dist;
 
-        chunkData thisChunk = chunksKnown.get(chunk);
+        int segX = ID.x * 16;
+        int segY = ID.y;
+        int segZ = ID.z * 16;
 
         for (int y = -15; y < 17; y++) {
             int intensity = 0;
             dist = Math.abs(y);
 
-            if (y + pY < 0) {
-                green = green + 17;
-                continue;
-            }
-            if (y + pY > 255) {
-                blue = blue + 16;
-                continue;
-            }
 
-            // block thisBlock = thisChunk.blockData[x][y + pY ][z];
-            boolean solid = thisChunk.getSolid(x, y + pY, z);
-            boolean intangible = thisChunk.getIntangible(x, y + pY, z);
-            boolean empty = thisChunk.getEmpty(x, y + pY, z);
-            boolean lit = thisChunk.getLit(x, y + pY, z);
+
+            boolean lit = false;
+            boolean solid = true;
+            boolean intangible = false;
+            boolean empty = false;
+
+            if (y + segY > 255) {
+                intangible = false;
+                solid = false;
+                empty = true;
+                lit = true;
+            } else if( y + segY >= 0 ) {
+
+                BlockPos pos = new BlockPos(segX + x, segY + y, segZ + z);
+
+                IBlockState state = world.getBlockState(pos);
+                Block blockID = state.getBlock();
+                lit = isLit(world, pos);
+    
+                if (state.isOpaqueCube() != true) {
+                    solid = false;
+    
+                    if (state.getCollisionBoundingBox(world, pos) == null) {
+                        intangible = true;
+    
+                        if (blockID.isAir(state, world, pos)) {
+                            empty = true;
+                        }
+                    }
+                }
+            }
 
             if (y > 1) {
                 dist--;
@@ -269,19 +213,16 @@ class mindshaftScanner {
         }
 
         color = clamp(red, 0, 255) << 16 | clamp(green, 0, 255) << 8 | clamp(blue, 0, 255);
-        // Mindshaft.logger.info(
-        // "chunk xz:" + chunk.x + "," + chunk.z +
-        // " xyz:" + x + "," + pY + "," + z + " rgb:" + red + "," + green + "," + blue);
         return color;
     }
 
-    layerSegment processSegment(World world, chunkID chunk, int pY) {
+    layerSegment processSegment(World world, segmentID ID) {
         layerSegment segment = new layerSegment(defaultColor);
 
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
 
-                int c = processColumn(world, chunk, x, z, pY);
+                int c = processColumn(world, ID, x, z);
                 segment.setColor(x, z, c);
             }
         }
@@ -289,73 +230,27 @@ class mindshaftScanner {
         return segment;
     }
 
-    layerSegment addLayerSegment(World world, chunkID chunk, int pY) {
-        layerSegment segment = processSegment(world, chunk, pY);
-        chunksKnown.get(chunk).layers.put(pY, segment);
+    layerSegment addLayerSegment(World world, segmentID ID) {
+        layerSegment segment = processSegment(world, ID);
+        segment.expiration = now + expiry + random.nextInt(expiryFudge);
+        segmentsKnown.put(ID, segment);
 
         return segment;
 
-        /*
-         * random.setSeed(chunk.x * chunk.z); layerSegment testSegment = new
-         * layerSegment(random.nextInt(0xFFFFFF)); return testSegment;
-         */
     }
 
-    layerSegment getLayerSegment(World world, chunkID chunk, int pY) {
-        chunkData thisChunk = getChunk(chunk);
-        if (thisChunk == null) {
-            return null;
-        }
-        layerSegment thisSegment = chunksKnown.get(chunk).layers.get(pY);
-        if (thisSegment == null) {
-            thisSegment = addLayerSegment(world, chunk, pY);
-        }
-
-        return thisSegment;
-    }
-
-    void scanChunk(World world, chunkID chunk) {
-        chunkData newChunk = new chunkData();
-        newChunk.expiration = now + expiry + random.nextInt(expiryFudge);
-        // Mindshaft.logger.info(now + ": new chunk: " + chunk.x + ", " + chunk.z + ",
-        // stale at: " + newChunk.expiration);
-        for (int x = 0; x < 16; x++) {
-            for (int y = 0; y < 256; y++) {
-                for (int z = 0; z < 16; z++) {
-
-                    BlockPos pos = new BlockPos(chunk.x * 16 + x, y, chunk.z * 16 + z);
-
-                    IBlockState state = world.getBlockState(pos);
-                    Block blockID = state.getBlock();
-
-                    boolean lit = isLit(world, pos);
-                    boolean solid = true;
-                    boolean intangible = false;
-                    boolean empty = false;
-
-                    if (state.isOpaqueCube() != true) {
-                        solid = false;
-
-                        if (state.getCollisionBoundingBox(world, pos) == null) {
-                            intangible = true;
-
-                            if (blockID.isAir(state, world, pos)) {
-                                empty = true;
-                            }
-                        }
-                    }
-
-                    newChunk.setLit(x, y, z, lit);
-                    newChunk.setSolid(x, y, z, solid);
-                    newChunk.setIntangible(x, y, z, intangible);
-                    newChunk.setEmpty(x, y, z, empty);
-
-                    // newChunk.blockData[x][y][z] = getBlock(world, chunk, x, y, z);
-                }
+    layerSegment getLayerSegment(World world, segmentID ID) {
+        layerSegment thisSegment = segmentsKnown.get(ID);
+        if (thisSegment == null || thisSegment.stale == true ) {
+            thisSegment = addLayerSegment(world, ID);
+        } else {
+            if(thisSegment.stale == false && thisSegment.expiration <= now ) {
+                thisSegment.stale = true;
+                thisSegment.expiration = now + forcedExpiry;
             }
         }
 
-        chunksKnown.put(chunk, newChunk);
+        return thisSegment;
     }
 
     void copyLayer(mindshaftRenderer renderer, layerSegment segment, int cX, int cZ) {
@@ -366,9 +261,6 @@ class mindshaftScanner {
             }
         }
     }
-
-    private static int pX;
-    private static int pZ;
 
     public void rasterizeLayers(World world, EntityPlayer player, mindshaftRenderer renderer, zoomState zoom) {
         if( currentTick == 0 ) {
@@ -400,36 +292,14 @@ class mindshaftScanner {
             if (cZ < 8 - radius || cZ > 8 + radius) {
                 continue;
             }
-            chunkID thisChunk = new chunkID(currentDim, cX + pcX, cZ + pcZ);
-            layerSegment thisSegment = getLayerSegment(world, thisChunk, (int) (player.posY - fudgeY));
+            segmentID ID = new segmentID(currentDim, cX + pcX, (int) (player.posY - fudgeY), cZ + pcZ);
+            layerSegment thisSegment = getLayerSegment(world, ID);
             if (thisSegment == null) {
                 continue;
             }
-            // Mindshaft.logger.info("cX: " + cX + ", cZ: " + cZ + "");
             copyLayer(renderer, thisSegment, cX, cZ);
         }
-        
 
-        /*
-        for (int cX = 0; cX < 16; cX++) {
-            if (cX < 8 - radius || cX > 8 + radius) {
-                continue;
-            }
-            for (int cZ = 0; cZ < 16; cZ++) {
-                if (cZ < 8 - radius || cZ > 8 + radius) {
-                    continue;
-                }
-
-                chunkID thisChunk = new chunkID(currentDim, cX + pcX, cZ + pcZ);
-                layerSegment thisSegment = getLayerSegment(world, thisChunk, (int) (player.posY - fudgeY));
-                if (thisSegment == null) {
-                    continue;
-                }
-                // Mindshaft.logger.info("cX: " + cX + ", cZ: " + cZ + "");
-                copyLayer(renderer, thisSegment, cX, cZ);
-            }
-        }
-        */
         if( currentTick++ >= mindshaftConfig.refreshdelay ) {
             renderer.refreshTexture();
             renderer.updatePos(pX, pZ);
@@ -441,6 +311,7 @@ class mindshaftScanner {
     public void processChunks(World world) {
         now = world.getTotalWorldTime();
         currentDim = world.provider.getDimension();
+        /*
         if (!requestedChunks.isEmpty()) {
             int cacheCount = 0;
             Iterator<chunkID> itr = requestedChunks.iterator();
@@ -449,14 +320,15 @@ class mindshaftScanner {
                 itr.remove();
             }
         }
-        if (!chunksKnown.isEmpty()) {
+        */
+        if (!segmentsKnown.isEmpty()) {
             int removeCount = 0;
-            Set<Map.Entry<chunkID, chunkData>> entryset = chunksKnown.entrySet();
-            Iterator<Map.Entry<chunkID, chunkData>> itr = entryset.iterator();
-            while (itr.hasNext() && removeCount++ <= mindshaftConfig.chunkrate) {
-                Map.Entry<chunkID, chunkData> entry = itr.next();
-                chunkData chunk = entry.getValue();
-                if (chunk.stale && chunk.expiration >= now) {
+            Set<Map.Entry<segmentID, layerSegment>> entryset = segmentsKnown.entrySet();
+            Iterator<Map.Entry<segmentID, layerSegment>> itr = entryset.iterator();
+            while (itr.hasNext() && removeCount++ <= mindshaftConfig.chunkrate * 10) {
+                Map.Entry<segmentID, layerSegment> entry = itr.next();
+                layerSegment segment = entry.getValue();
+                if (segment.stale && segment.expiration <= now) {
                     itr.remove();
                 }
             }
